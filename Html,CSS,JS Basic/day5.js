284bc805b368f3f833a158029c6ddd81e33fb53c
Sonali Gaikwad <sonaligaikwad3690@gmail.com>
2026-06-19 11:29:59 +0530

    Create day5.js

---
+++ Html,CSS,JS Basic/day5.js
@@ -0,0 +1,86 @@
+console.log("from external JS")
+
+var mi = "rohit" ;   // declaration is allowed
+
+micap = "rohit sharma" ;   // re-assignment is allowed
+
+// ---------- 1000 lines of  code 
+
+var mi = "hardik pandya"  ;    // re-declaration is also allowed {not recommended}
+
+// 2000 lines code....
+
+console.log("captain = " +mi)  // thatswhy we dont use var now a days in industry level projects
+
+// let
+
+let rcb_batsmen = 'virat' ;  // declaration is allowed
+
+rcb_batsmen = "virat kohli" ;   // re-assignment is allowed
+
+// 1000 lines
+
+ // let rcb_batsmen = "rajat patidar" ; // re-declaration is not allowed {recommended in industry}
+
+console.log("rcb_batsmen = " , rcb_batsmen)
+
+
+const csk = "M.S Dhoni" ;   // declaration is allowed
+
+// csk = "ruturaj gaikwad" // re-assignment is not allowed
+
+// const csk = "jadeja"  // re-decalration is never allowed... 
+
+console.log("csk = " , csk)
+
+
+// can i create a const variable without assigning 
+// the value and then i will assign the value on the next line ?  
+
+
+const rr ; 
+
+// rr = "Vaibhav S"
+
+console.log("rr player = " , rr)
+
+
+// explore this as well as hoisting...
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Js is a basic scripting language use to create dynamic web pages : 
+
+
+// var , let , const  
+// data types
+// variables , keywords 
+// functions , return 
+// operators 
+// control statements
+// array , String
+// document -- DOM 
