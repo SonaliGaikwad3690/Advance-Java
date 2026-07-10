ccd253323989937df1bb7d64e99d24fcb7a4c875
Sonali Gaikwad <sonaligaikwad3690@gmail.com>
2026-07-10 10:22:05 +0530

    Create Multi.jsx

---
+++ React/Projects/my-props-app/Multi.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Multi = (props) => {
+
+
+    // let acceptValue=multi();
+  return(
+
+   <div>
+
+
+ <h2>Multiply = {props.Multi}</h2>
+
+  {/* <h2>Multiply = {acceptValue}</h2>
+  <h2>Multiply  direct call = {multi()}</h2> */}
+
+
+</div>
+
+
+);
+}
+
+export default Multi
