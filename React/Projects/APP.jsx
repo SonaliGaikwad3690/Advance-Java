7fc08825080fb9de118fba0c9acefb956c1fa0cc
Sonali Gaikwad <sonaligaikwad3690@gmail.com>
2026-07-11 21:06:38 +0530

    Create APP.jsx

---
+++ React/Projects/APP.jsx
@@ -0,0 +1,14 @@
+import Header from "./Header";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <h2>Welcome to React Components</h2>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
