9a0bea94a55b96dea320b8e930c477cda3cfa9d7
Sonali Gaikwad <sonaligaikwad3690@gmail.com>
2026-07-21 14:45:15 +0530

    Create Kids.jsx

---
+++ React/Projects/my-route-app/Kids.jsx
@@ -0,0 +1,18 @@
+import react from 'react'
+
+
+const Kids = ()=> {
+
+  return (
+    <div>
+      <center>
+
+        <h1>Kids Contents...!</h1>
+        
+      </center>
+           
+    </div>
+  )
+}
+
+export default Kids
