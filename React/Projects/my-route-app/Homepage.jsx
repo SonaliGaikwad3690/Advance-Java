a0fbef6df91838e7f9c84a2434d9680169f3cde0
Sonali Gaikwad <sonaligaikwad3690@gmail.com>
2026-07-21 14:44:34 +0530

    Create Homepage.jsx

---
+++ React/Projects/my-route-app/Homepage.jsx
@@ -0,0 +1,18 @@
+import react from 'react'
+
+
+const Homepage = ()=> {
+
+  return (
+    <div>
+      <center>
+
+        <h1>Homepage Contents...!</h1>
+        
+      </center>
+           
+    </div>
+  )
+}
+
+export default Homepage
