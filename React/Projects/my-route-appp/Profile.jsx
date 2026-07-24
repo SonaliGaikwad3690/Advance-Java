14b2393609171bbaa59ed7544f490b7a9f695661
Sonali Gaikwad <sonaligaikwad3690@gmail.com>
2026-07-24 10:28:20 +0530

    Create Profile.jsx

---
+++ React/Projects/my-route-appp/Profile.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+const Profile = () =>{
+
+    return(
+        <div>
+            <center>
+                  
+                  <h1>Profile Page</h1>
+            </center>
+        </div>
+    )
+}
+ export default Profile
