2fa778b840c6c6bbe2a0717c6ce889912c577b3d
Sonali Gaikwad <sonaligaikwad3690@gmail.com>
2026-08-12 18:01:50 +0530

    Create Profile.jsx

---
+++ React/Projects/My-Context-App/Profile.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import User from './User'
+
+const Profile = () => {
+  return (
+    <div>
+
+<center>
+    <h2>Profile opend...!</h2>
+       <User/>
+</center>
+    </div>
+  )
+}
+
+export default Profile
