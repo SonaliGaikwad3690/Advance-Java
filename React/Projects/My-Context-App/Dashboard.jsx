c273ea578bd7b2475dee45bda7729e282168aeeb
Sonali Gaikwad <sonaligaikwad3690@gmail.com>
2026-08-12 18:01:30 +0530

    Add Dashboard component with Profile integration

---
+++ React/Projects/My-Context-App/Dashboard.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Profile from './Profile'
+
+const Dashboard = () => {
+  return (
+    <div>
+      <center>
+
+        <h2>Welcom to Dashboard</h2>
+         <Profile/>
+      </center>
+
+      
+
+    </div>
+  )
+}
+
+export default Dashboard
